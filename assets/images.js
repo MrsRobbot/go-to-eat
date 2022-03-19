0c574f11efcf165302dad7037163021bf383a625
oracle2_dev <assia-jean.ngoran@wealthtechinnovations.com>
2022-03-19 18:56:34 +0000

    Design 1ere page

---
+++ assets/images.js
@@ -0,0 +1,18 @@
+const logo1 = require("../assets/images/LOGO-01.svg");
+const logo2 = require("../assets/images/LOGO-02.svg");
+const vector1 = require("../assets/images/VECTOR-01.svg");
+const vector2 = require("../assets/images/VECTOR-02.svg");
+const vector3 = require("../assets/images/VECTOR-03.svg");
+const vector4 = require("../assets/images/VECTOR-04.svg");
+const eat = require('../assets/images/eat.svg')
+
+
+export default {
+    logo1,
+    logo2,
+    vector1,
+    vector2,
+    vector3,
+    vector4,
+    eat
+}
---
+++ pages/Welcome.js
@@ -0,0 +1,58 @@
+import { StyleSheet, Text, View, Image, Button } from 'react-native'
+import React from 'react'
+import images from '../assets/images'
+import { Button as ButtonPaper } from 'react-native-paper';
+
+const Welcome = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.View}>
+        <Image
+            source={images.logo2}
+            style={{
+                height:150,
+                width:150
+            }}
+        />
+        <Text style={styles.title}>Bienvenue sur Go To Eat</Text>
+        <Text style={styles.paragraph}>La première plateforme Suisse de Restauration par Abonnement.</Text>
+
+        <Image
+            source={images.eat}
+            style={{
+                width:200,
+                height:200,
+                marginBottom:"12%"
+            }}
+        />
+        <ButtonPaper mode='contained' icon="arrow-right">GO</ButtonPaper>
+      </View>
+    </View>
+  )
+}
+
+export default Welcome
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:"white",
+        padding:10
+    },
+    View:{
+        flex:1,
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    title:{
+        fontSize:23,
+        fontWeight:"bold",
+        marginBottom:"10%"
+    },
+    paragraph:{
+        fontSize:19,
+        textAlign:"center",
+        color:"gray",
+        marginBottom:"15%"
+    }
+})
